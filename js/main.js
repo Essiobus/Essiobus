5c1095e26ab20ff75fc180c02d133d4891b08e1a
khnaf <43346502+khnaf@users.noreply.github.com>
2018-11-12 17:28:05 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-Essiobus = new phaserGame(1280,720);
+
+var game = new Phaser.Game(config);
+
+var config = {
+    width: 1280,
+    height: 720,
+    type: Phaser.WEBGL,
+    scene: {
+        preload: preload,
+        create: create,
+    }
+};
+
+var objects = {};
+function preload (){
+    
+}
+
+function create (){
+    
+    objects.setBackgroundColor('rgba(255, 0, 0, 0.5)');
+}
+
+    
+
+
+
+
+                               
